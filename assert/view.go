39d5239a1c074ac1ef3f9f957a93c016d47a28ca
Sergio Andres Virviescas Santana <developersavsgio@gmail.com>
2021-01-09 17:25:17 +0100

    Upgrade dependencies

--- assert/view.go
+++ assert/view.go
@@ -8,7 +8,7 @@ import (
 )
 
 func View(t *testing.T, req *fasthttp.Request, fnView atreugo.View, assertFn func(resp *fasthttp.Response)) {
-	s := atreugo.New(atreugo.Config{LogLevel: "fatal"})
+	s := atreugo.New(atreugo.Config{})
 	s.Path(string(req.Header.Method()), string(req.URI().PathOriginal()), fnView)
 
 	Server(t, req, s, assertFn)
